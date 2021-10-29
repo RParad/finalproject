446c8788962cd074eea2593a076faeebddeac3d0
Ryan <Ryan2@MacBook-Air.local>
2021-10-29 15:12:53 +0100

    First Commit

---
+++ src/test/java/com/software/institute/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.institute.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

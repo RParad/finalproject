e826a0a8b6d4ebf27ef176741e4a4bbee4c26077
Ryan <Ryan2@MacBook-Air.local>
2021-11-04 14:00:32 +0000

    New add

--- src/main/java/com/software/institute/finalproject/FinalprojectApplication.java
+++ src/main/java/com/software/institute/finalproject/FinalprojectApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 
 @SpringBootApplication
 @RestController
@@ -25,10 +23,10 @@ public class FinalprojectApplication {
 		return filmRepository.findAll();
 	}
 
-	@GetMapping("/filmsByID")
-	public @ResponseBody
-	Optional<Film> getAllUsers4() {
-		return filmRepository.findById(6);
+	@GetMapping("/films/{film_id}")
+	public @ResponseBody Film findByID(@PathVariable("film_id")int film_id) {
+	Film filmByID = filmRepository.findById(film_id).orElse(null);
+		return filmByID;
 	}
 
 	///////////////////// Create function \\\\\\\\\\\\
@@ -36,19 +34,26 @@ public class FinalprojectApplication {
 	@PostMapping("/addFilm")
 	public @ResponseBody
 	String newFilm(@RequestParam int film_id, @RequestParam String title, @RequestParam int length, @RequestParam int language_id) {
-
 		Film savedFilm = new Film(film_id, title, length, language_id);
 		filmRepository.save(savedFilm);
 		return "Film Has Been Added";
 	}
 
 
-	/*////////////////////////Delete\\\\\\\\\\\\\\\\\\\\\\\\
-	@DeleteMapping("/removeFilm{film_id}")
-	public String deleteFilm(@PathVariable("film_id") int film_id) {
+	////////////////////////Delete\\\\\\\\\\\\\\\\\\\\\\\\
+	@DeleteMapping("/removeFilm/{film_id}")
+	public @ResponseBody String deleteFilm(@PathVariable("film_id") int film_id) {
 		filmRepository.deleteById(film_id);
 		return "Film deleted.";
-	}*/
+	}
+
+
+	//////////////////////Update\\\\\\\\\\\\\\\\\\\\\\\\\\
+	@PutMapping("/films/{film_id}")
+	public @ResponseBody String editFilm(@PathVariable ("film_id") int film_id) {
+		return "Film has been Changed";
+	}
+
 
 	//@GetMapping("/actor")
 	//public @ResponseBody Iterable<Actor> getAllActors() {
--- src/test/java/com/software/institute/finalproject/FilmTest.java
+++ src/test/java/com/software/institute/finalproject/FilmTest.java
@@ -22,5 +22,19 @@ public class FilmTest {
         Film testFilm = new Film(1, "Avatar", 134, 6);
         assertEquals("Avatar", testFilm.getTitle(), Title);
     }
-    
+    @Test
+    public void testGetFilmLengthMethod(){
+        Film testFilm = new Film (1, "Avatar", 128, 6);
+        assertEquals(128, testFilm.getLength(), "The film is 128 mins long");
+    }
+    @Test
+    public void testGetFilmIDMethod() {
+        Film testFilm = new Film(1,"Avatar", 128, 1);
+        assertEquals(1,testFilm.getFilm_id(), "This Sounds about right");
+    }
+    @Test
+    public void testGetlanguageIDMethod() {
+        Film testFilm = new Film(1,"Avatar", 128, 1);
+        assertEquals(1,testFilm.getLanguage_id(), "This Sounds about right, again?");
+    }
 }
